0cea4599f400119ec2206d7553a5e791f5c57ae2
ebeagusamuel <samuelebeagu@gmail.com>
2020-10-20 23:14:08 +0100

    implement the operate method and fix linters error

--- src/logic/calculate.js
+++ src/logic/calculate.js
@@ -1,69 +1,64 @@
+import operate from './operate';
+
 const calculate = (
   calObj = {
-    total: "",
-    next: "",
-    operation: "",
+    total: '',
+    next: '',
+    operation: '',
   },
-  btnName
+  btnName,
 ) => {
-  let {total, next, operation} = calObj;
-  const numbers = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0']
-  const mathSymbols = ['+', '-', 'X', '÷', '%']
+  const data = calObj;
+  const { total, next, operation } = data;
+  const numbers = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0'];
+  const mathSymbols = ['+', '-', 'X', '÷', '%'];
 
-  if(btnName === 'AC'){
-    calObj.total = ''
-    calObj.next = ''
-    calObj.operation = ''
-
-    return calObj
+  if (btnName === 'AC') {
+    data.total = '';
+    data.next = '';
+    data.operation = '';
   }
 
-  if(btnName === '+/-'){
-    if(total && !next){
-      calObj.total = total * (-1)
+  if (btnName === '+/-') {
+    if (total && !next) {
+      data.total = total * -1;
     }
-    if(next){
-      calObj.next = next * (-1)
+    if (next) {
+      data.next = next * -1;
     }
-
-    return calObj
   }
 
-  if(btnName === '.'){
-    if(!total && !next){
-      calObj.total += btnName
+  if (btnName === '.') {
+    if (!total && !next) {
+      data.total += btnName;
     }
-    if(next){
-      calObj.next += btnName
+    if (total && !next) {
+      data.next += btnName;
     }
-
-    return calObj
   }
 
-  if(numbers.includes(btnName)){
-    if(!total || (total && !operation)){
-      calObj.total += btnName
+  if (numbers.includes(btnName)) {
+    if (!total || (total && !operation)) {
+      data.total += btnName;
     }
-    if(total && operation){
-      calObj.next += btnName
+    if (total && operation) {
+      data.next += btnName;
     }
-
-    return calObj
   }
 
-  if(mathSymbols.includes(btnName)){
-    calObj.operation = btnName
-
-    return calObj
+  if (mathSymbols.includes(btnName) && total) {
+    data.operation = btnName;
   }
 
-  if(btnName === '='){
-    let numberOne = calObj.total
-    let numberTwo = calObj.next
-    let mathSymbol = calObj.operation
+  if (btnName === '=') {
+    const numberOne = data.total;
+    const numberTwo = data.next;
+    const mathSymbol = data.operation;
 
-    return operate(numberOne, numberTwo, mathSymbol)
+    return operate(numberOne, numberTwo, mathSymbol);
   }
+
+  return data;
 };
 
-console.log(calculate({total: '890', next: '', operation: 'X'}, '5'))
+export default calculate;
---
+++ src/logic/operate.js
@@ -0,0 +1,33 @@
+import Big from 'big.js';
+
+const operate = (numberOne, numberTwo, operation) => {
+  let ans;
+
+  const one = Big(numberOne);
+  let two;
+  if (numberTwo === '') {
+    two = 0;
+  } else {
+    two = Big(numberTwo);
+  }
+
+  if (operation === 'X') {
+    ans = one * two;
+  }
+  if (operation === '+') {
+    ans = one + two;
+  }
+  if (operation === '-') {
+    ans = one - two;
+  }
+  if (operation === '÷') {
+    ans = one / two;
+  }
+  if (operation === '%') {
+    ans = one / 100;
+  }
+
+  return ans;
+};
+
+export default operate;
