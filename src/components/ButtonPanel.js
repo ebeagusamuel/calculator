bff7909c82a9f9759e20fff0ab81305633c5eed4
ebeagusamuel <samuelebeagu@gmail.com>
2020-10-20 03:24:02 +0100

    refactor ButtonPanel component to remove redundant code

--- src/components/ButtonPanel.js
+++ src/components/ButtonPanel.js
@@ -4,44 +4,36 @@ import Button from './Button';
 const renderButton = name => <Button name={name} />;
 
 const ButtonPanel = () => {
-  const buttonNames = {
-    group1: ['AC', '+/-', '%', '÷'],
-    group2: ['7', '8', '9', 'X'],
-    group3: ['4', '5', '6', '-'],
-    group4: ['1', '2', '3', '+'],
-    group5: ['0', '.', '='],
-  };
-
   return (
     <div>
       <div id="group1">
-        {renderButton(buttonNames.group1[0])}
-        {renderButton(buttonNames.group1[1])}
-        {renderButton(buttonNames.group1[2])}
-        {renderButton(buttonNames.group1[3])}
+        {renderButton('AC')}
+        {renderButton('+/-')}
+        {renderButton('%')}
+        {renderButton('÷')}
       </div>
       <div id="group2">
-        {renderButton(buttonNames.group2[0])}
-        {renderButton(buttonNames.group2[1])}
-        {renderButton(buttonNames.group2[2])}
-        {renderButton(buttonNames.group2[3])}
+        {renderButton('7')}
+        {renderButton('8')}
+        {renderButton('9')}
+        {renderButton('X')}
       </div>
       <div id="group3">
-        {renderButton(buttonNames.group3[0])}
-        {renderButton(buttonNames.group3[1])}
-        {renderButton(buttonNames.group3[2])}
-        {renderButton(buttonNames.group3[3])}
+        {renderButton('4')}
+        {renderButton('5')}
+        {renderButton('6')}
+        {renderButton('-')}
       </div>
       <div id="group4">
-        {renderButton(buttonNames.group4[0])}
-        {renderButton(buttonNames.group4[1])}
-        {renderButton(buttonNames.group4[2])}
-        {renderButton(buttonNames.group4[3])}
+        {renderButton('1')}
+        {renderButton('2')}
+        {renderButton('3')}
+        {renderButton('+')}
       </div>
       <div id="group5">
-        {renderButton(buttonNames.group5[0])}
-        {renderButton(buttonNames.group5[1])}
-        {renderButton(buttonNames.group5[2])}
+        {renderButton('0')}
+        {renderButton('.')}
+        {renderButton('=')}
       </div>
     </div>
   );
