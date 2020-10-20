71f5e2467374acf22c1ceaf5db89edbf3eb950b8
ebeagusamuel <samuelebeagu@gmail.com>
2020-10-20 03:26:59 +0100

    fix lint errors

--- src/components/ButtonPanel.js
+++ src/components/ButtonPanel.js
@@ -3,40 +3,38 @@ import Button from './Button';
 
 const renderButton = name => <Button name={name} />;
 
-const ButtonPanel = () => {
-  return (
-    <div>
-      <div id="group1">
-        {renderButton('AC')}
-        {renderButton('+/-')}
-        {renderButton('%')}
-        {renderButton('÷')}
-      </div>
-      <div id="group2">
-        {renderButton('7')}
-        {renderButton('8')}
-        {renderButton('9')}
-        {renderButton('X')}
-      </div>
-      <div id="group3">
-        {renderButton('4')}
-        {renderButton('5')}
-        {renderButton('6')}
-        {renderButton('-')}
-      </div>
-      <div id="group4">
-        {renderButton('1')}
-        {renderButton('2')}
-        {renderButton('3')}
-        {renderButton('+')}
-      </div>
-      <div id="group5">
-        {renderButton('0')}
-        {renderButton('.')}
-        {renderButton('=')}
-      </div>
+const ButtonPanel = () => (
+  <div>
+    <div id="group1">
+      {renderButton('AC')}
+      {renderButton('+/-')}
+      {renderButton('%')}
+      {renderButton('÷')}
     </div>
-  );
-};
+    <div id="group2">
+      {renderButton('7')}
+      {renderButton('8')}
+      {renderButton('9')}
+      {renderButton('X')}
+    </div>
+    <div id="group3">
+      {renderButton('4')}
+      {renderButton('5')}
+      {renderButton('6')}
+      {renderButton('-')}
+    </div>
+    <div id="group4">
+      {renderButton('1')}
+      {renderButton('2')}
+      {renderButton('3')}
+      {renderButton('+')}
+    </div>
+    <div id="group5">
+      {renderButton('0')}
+      {renderButton('.')}
+      {renderButton('=')}
+    </div>
+  </div>
+);
 
 export default ButtonPanel;
